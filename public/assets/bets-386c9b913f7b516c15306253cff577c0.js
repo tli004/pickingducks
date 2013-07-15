00a5f66c85f0d9fe776777f865efa4f127442c45
Chris Wood <cdub@ubuntu.(none)>
2013-07-15 10:19:57 -0700

    precompiled ALL assets locally

---
+++ public/assets/bets-386c9b913f7b516c15306253cff577c0.js
@@ -0,0 +1,1 @@
+var parlay_odds=[2.6,6,10,20,40,75,150,300,700,1100],num_games=0;$(function(){$("#bet_tabs").tabs(),$(".number_input").change(function(){num_games>1?$(".winning_payout").text("$"+(parseInt($(this).val())*parlay_odds[num_games-2]).toFixed(2).toString()):$(".winning_payout").text("N/A")})});
---
+++ public/assets/users.js
@@ -0,0 +1,1 @@
+$(function(){$("#profile_tabs").tabs(),$(".upload_image").click(function(){$(".hidden_upload_field").click()}),$(".hidden_upload_field").change(function(){$(this).closest("form").submit()})});
